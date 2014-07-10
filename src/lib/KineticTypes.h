ee618f010b4386383eb20d1c1ee3e51e37a2673f
Greg Williams <greg.williams@atomicobject.com>
2014-07-10 15:39:18 -0400

    Updated OS-specific setting of MAX_HOST_NAME to process OSX first, before falling back to generic UNIX

--- src/lib/KineticTypes.h
+++ src/lib/KineticTypes.h
@@ -17,22 +17,22 @@
     #include <winsock2.h>
     #define HOST_NAME_MAX (MAX_COMPUTERNAME_LENGTH+1)
 #else
-    #if defined(__unix__)
-        // Some Linux environments require this, although not all, but it's benign.
-        #define _BSD_SOURCE
-        #include <unistd.h>
+    #if defined(__APPLE__)
+        // MacOS does not defined HOST_NAME_MAX so fall back to the POSIX value.
+        // We are supposed to use sysconf(_SC_HOST_NAME_MAX) but we use this value
+        // to size an automatic array and we can't portably use variables for that.
+        #define HOST_NAME_MAX _POSIX_HOST_NAME_MAX
     #else
-        #if defined(__APPLE__)
-            // MacOS does not defined HOST_NAME_MAX so fall back to the POSIX value.
-            // We are supposed to use sysconf(_SC_HOST_NAME_MAX) but we use this value
-            // to size an automatic array and we can't portably use variables for that.
-            #define HOST_NAME_MAX _POSIX_HOST_NAME_MAX
+        #if defined(__unix__)
+            // Some Linux environments require this, although not all, but it's benign.
+            #define _BSD_SOURCE
+            #include <unistd.h>
         #else
             #if !defined(HOST_NAME_MAX)
                 #define HOST_NAME_MAX 256
             #endif // HOST_NAME_MAX
-        #endif // __APPLE__
-    #endif  // __unix__
+        #endif  // __unix__
+    #endif // __APPLE__
 #endif  // WIN32
 
 #define KINETIC_PORT 8213
