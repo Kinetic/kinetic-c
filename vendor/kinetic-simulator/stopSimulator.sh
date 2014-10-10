5185e8052fe418ae7ef21c1c7f0508a0c5098e41
Greg Williams <greg.williams@atomicobject.com>
2014-10-10 09:29:48 -0700

    Fixed kinetic simulator shutdown script

--- vendor/kinetic-simulator/stopSimulator.sh
+++ vendor/kinetic-simulator/stopSimulator.sh
@@ -1,2 +1,3 @@
 # Ensure stray simulators are not still running
-ps -ef | grep kinetic-simulator | grep -v 'grep' | tr -s ' ' | cut -d ' ' -f 3 | xargs kill $1
+ps -ef | grep kinetic-simulator | grep -v 'grep' | tr -s ' ' | cut -d ' ' -f 2 | xargs kill $1
+echo Killed all kinetic simulators!
