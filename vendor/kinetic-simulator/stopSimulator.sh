412db999689f20a4b50005b4b32ab3085e2ec9a9
Greg Williams <greg.williams@atomicobject.com>
2015-01-14 09:34:07 -0500

    Updated make stop_simulator to properly stop all simulators when multiple are running

--- vendor/kinetic-simulator/stopSimulator.sh
+++ vendor/kinetic-simulator/stopSimulator.sh
@@ -1,20 +1,14 @@
 #! /usr/bin/env bash
 
-# Ensure stray simulators are not still running
-_SIMS=`ps -ef | grep "kinetic-simulator/kinetic-simulator-" | grep -v 'grep' | sed -e 's/^ *//' -e 's/ *$//' | tr -s ' '`
-echo "Kinetic Simulators Still Alive"
-echo "------------------------------"
-echo $_SIMS
-echo
-
+# Kill any simulators running
+_SIMS=`ps -ef | grep "kinetic-simulator/kinetic-simulator-" | grep -v 'grep' | sed -e 's/^ *//' -e 's/ *$//' | tr -s ' ' | cut -d ' ' -f 2`
 if [[ ! -z $_SIMS ]]; then
-  _KINETIC_SIM=`echo $_SIMS | cut -d ' ' -f 2`
-  echo Simulator PIDs: $_KINETIC_SIM
-  if [[ ! -z $_KINETIC_SIM ]]; then
-    sleep 2
-    echo Killing kinetic-simulator with PIDs: $_KINETIC_SIM
-    kill $_KINETIC_SIM
-    echo Killed all kinetic simulators!
-    sleep 2
-  fi
+  for sim in $_SIMS
+  do
+    if [[ ! -z $sim ]]; then
+      echo Killing kinetic-simulator \(pid=$sim\)
+      kill $sim
+    fi
+  done
+  echo All simulators have been terminated!
 fi
