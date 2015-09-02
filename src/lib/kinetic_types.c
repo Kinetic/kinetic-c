258b275009488f6081bde988226a9f7164c32606
Daisy Hsiung <huiwendaisy@gmail.com>
2015-09-02 11:46:02 -0700

    correct the mapping between KineticMessage and kinetic protocol enum

--- src/lib/kinetic_types.c
+++ src/lib/kinetic_types.c
@@ -73,8 +73,8 @@ const char* Kinetic_GetStatusDescription(KineticStatus status)
 }
 
 const char* KineticMessageTypeNames[33] =
-{
-    "<UNKNOWN>"
+{  
+    "<UNKNOWN>",
     "GET_RESPONSE",
     "GET",
     "PUT_RESPONSE",
@@ -106,45 +106,109 @@ const char* KineticMessageTypeNames[33] =
     "MEDIASCAN_RESPONSE",
     "MEDIASCAN",
     "MEDIAOPTIMIZE_RESPONSE",
-    "MEDIAOPTIMIZE"
+    "MEDIAOPTIMIZE",
 };
 
 const char* KineticMessageType_GetName(KineticMessageType type)
 {
     switch(type) {
     case KINETIC_MESSAGE_TYPE_GET_RESPONSE:
+	return "GET_RESPONSE";
+	break;
     case KINETIC_MESSAGE_TYPE_GET:
+	return "GET";
+	break;
     case KINETIC_MESSAGE_TYPE_PUT_RESPONSE:
+	return "PUT_RESPONSE";
+	break;
     case KINETIC_MESSAGE_TYPE_PUT:
+	return "PUT";
+	break;
     case KINETIC_MESSAGE_TYPE_DELETE_RESPONSE:
+	return  "DELETE_RESPONSE";
+	break;
     case KINETIC_MESSAGE_TYPE_DELETE:
+	return "DELETE";
+	break;
     case KINETIC_MESSAGE_TYPE_GETNEXT_RESPONSE:
+	return "GETNEXT_RESPONSE";    
+	break;
     case KINETIC_MESSAGE_TYPE_GETNEXT:
+	return  "GETNEXT";
+	break;
     case KINETIC_MESSAGE_TYPE_GETPREVIOUS_RESPONSE:
+	return  "GETPREVIOUS_RESPONSE";
+	break;
     case KINETIC_MESSAGE_TYPE_GETPREVIOUS:
+	return "GETPREVIOUS";
+	break;
     case KINETIC_MESSAGE_TYPE_GETKEYRANGE_RESPONSE:
+	return "GETKEYRANGE_RESPONSE";
+	break;
     case KINETIC_MESSAGE_TYPE_GETKEYRANGE:
+	return "GETKEYRANGE";    
+	break;
     case KINETIC_MESSAGE_TYPE_GETVERSION_RESPONSE:
+	return "GETVERSION_RESPONSE";
+	break;
     case KINETIC_MESSAGE_TYPE_GETVERSION:
+	return "GETVERSION";
+	break;
     case KINETIC_MESSAGE_TYPE_SETUP_RESPONSE:
+	return "SETUP_RESPONSE";
+	break;
     case KINETIC_MESSAGE_TYPE_SETUP:
+	return "SETUP";
+	break;
     case KINETIC_MESSAGE_TYPE_GETLOG_RESPONSE:
+	return "GETLOG_RESPONSE";
+	break;
     case KINETIC_MESSAGE_TYPE_GETLOG:
+	return "GETLOG";
+	break;
     case KINETIC_MESSAGE_TYPE_SECURITY_RESPONSE:
+	return "SECURITY_RESPONSE";
+	break;
     case KINETIC_MESSAGE_TYPE_SECURITY:
+	return "SECURITY";
+	break;
     case KINETIC_MESSAGE_TYPE_PEER2PEERPUSH_RESPONSE:
+	return "PEER2PEERPUSH_RESPONSE";
+	break;
     case KINETIC_MESSAGE_TYPE_PEER2PEERPUSH:
+	return "PEER2PEERPUSH";
+	break;
     case KINETIC_MESSAGE_TYPE_NOOP_RESPONSE:
+	return "NOOP_RESPONSE";
+	break;
     case KINETIC_MESSAGE_TYPE_NOOP:
+	return "NOOP";
+	break;
     case KINETIC_MESSAGE_TYPE_FLUSHALLDATA_RESPONSE:
+	return "FLUSHALLDATA_RESPONSE";
+	break;
     case KINETIC_MESSAGE_TYPE_FLUSHALLDATA:
+	return "FLUSHALLDATA";
+	break;
+
     case KINETIC_MESSAGE_TYPE_PINOP_RESPONSE:
+	return "PINOP_RESPONSE";
+	break;
     case KINETIC_MESSAGE_TYPE_PINOP:
+	return "PINOP";
+	break;
     case KINETIC_MESSAGE_TYPE_MEDIASCAN_RESPONSE:
+	return "MEDIASCAN_RESPONSE";
+	break;
     case KINETIC_MESSAGE_TYPE_MEDIASCAN:
+	return "MEDIASCAN";
+	break;
     case KINETIC_MESSAGE_TYPE_MEDIAOPTIMIZE_RESPONSE:
+	return "MEDIAOPTIMIZE_RESPONSE";
+	break;
     case KINETIC_MESSAGE_TYPE_MEDIAOPTIMIZE:
-      return KineticMessageTypeNames[type];
+	return "MEDIAOPTIMIZE";
+	break;
     default:
     case KINETIC_MESSAGE_TYPE_INVALID:
       return KineticMessageTypeNames[0];
